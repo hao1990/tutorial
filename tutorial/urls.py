3230fa98e0cc0dd73222d77ca082148f0b8736f5
duhao <duhaoios@163.com>
2019-05-17 15:59:13 +0800

    part-2 serialization

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,12 +19,13 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users',views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users',views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    url(r'^',include(router.urls)),
-    url(r'^api-auth/',include('rest_framework.urls', namespace='rest_form')),
+    # url(r'^',include(router.urls)),
+    # url(r'^api-auth/',include('rest_framework.urls', namespace='rest_form')),
+    url(r'^', include('snippets.urls')),
 ]
